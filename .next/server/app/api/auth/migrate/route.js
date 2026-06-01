5c4b220b11cd071e08d60f1a797758e25c05e314
anjaliPatel06 <apatel83984@gmail.com>
2026-06-01 23:02:23 +0530

    Added remaining files

---
+++ .next/server/app/api/auth/migrate/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/auth/migrate/route.js")
+R.c("server/chunks/[root-of-the-server]__c4d88e7a._.js")
+R.c("server/chunks/[root-of-the-server]__0015c0ff._.js")
+R.c("server/chunks/_next-internal_server_app_api_auth_migrate_route_actions_663ec893.js")
+R.m(71161)
+module.exports=R.m(71161).exports
---
+++ .next/server/app/api/reports/[ticketId]/route.js
@@ -0,0 +1,7 @@
+var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/reports/[ticketId]/route.js")
+R.c("server/chunks/[root-of-the-server]__bc1b0284._.js")
+R.c("server/chunks/[root-of-the-server]__0015c0ff._.js")
+R.c("server/chunks/[root-of-the-server]__45acce4d._.js")
+R.c("server/chunks/_next-internal_server_app_api_reports_[ticketId]_route_actions_58ec603a.js")
+R.m(46041)
+module.exports=R.m(46041).exports
